bbe255a7f1e9faf475e2eb19dde5a1cce1d54914
faris17-meet <faris17@meet.mit.edu>
2016-11-27 20:00:40 +0200

    Update answers.js

--- answers.js
+++ answers.js
@@ -8,4 +8,32 @@
 
 ///4.  LOOPS
 
+var temp = 98.6;
+var temp = temp + 5;
+var temp = temp * 3 ;
+var temp = temp/20 ;
 
+
+var days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
+days_of_the_week[0] = "domingo";
+days_of_the_week[1] = "lunes";
+days_of_the_week[2] = "martes";
+days_of_the_week[3] = "miercoles";
+days_of_the_week[4] = "Jueves";
+days_of_the_week[5] = "viernes";
+days_of_the_week[6] = "sabado";
+ 
+var iceCream = {"Vanila":1.99 , "Chocolate":2.45 , "Strawberry":2.25 };
+iceCream["Chocolate"] = 0.99;
+console.log(iceCream['Chocolate']);
+
+function DoIHaveToGoToSchoolToday(number){
+var DaysOfTheWeek = {0:"Sunday", 1:"Monday", 2:"Tuesday", 3:"Wednsday", 4:"Thursday", 5:"Friday", 6:"Saturday"}
+if(number = 5 ){console.log("today is "+DaysOfTheWeek[5]+"- its not a school day:)");
+} 
+else{console.log("today is "+DaysOfTheWeek[]+"
+
+}
+};
+
+DoIHaveToGoToSchoolToday(5);
